0c6eb8ce8fab4b71d76c5deb2d640d2a6f224c0e
Roberts Jakovlevs <roberts7531@gmail.com>
2020-07-16 16:15:36 +0300

    Fixed users.html userId -> id removed version requirement from pom.xml Thymeleaf working!!

--- src/main/java/teamg/spring/boot/controller/UserController.java
+++ src/main/java/teamg/spring/boot/controller/UserController.java
@@ -3,15 +3,17 @@ package teamg.spring.boot.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import teamg.spring.boot.model.User;
 import teamg.spring.boot.service.UserService;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController("/api/rest/User.svc")
 public class UserController {
@@ -28,14 +30,19 @@ public class UserController {
     }
 
     @GetMapping("/users")
-    public List<User> getAllUsers(){
+
+    public  Model getAllUsers(Model model){
+
         LOG.info("getAllUsers");
-        return userService.getAllUsers();
+        model.addAttribute("listUsers", userService.getAllUsers());
+        return model;
+
     }
 
 
     @GetMapping("/")
     public String viewHomePage(Model model){
+        //whats going on here?
         model.addAttribute("listUsers", userService.getAllUsers());
         return "users";
     }
