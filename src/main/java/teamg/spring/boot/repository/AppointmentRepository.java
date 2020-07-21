9195da9477fec38b0af5b19b9406f91bb184f780
Roberts Jakovlevs <roberts7531@gmail.com>
2020-07-21 19:25:54 +0300

    calendar shows every event day now

--- src/main/java/teamg/spring/boot/repository/AppointmentRepository.java
+++ src/main/java/teamg/spring/boot/repository/AppointmentRepository.java
@@ -14,6 +14,6 @@ public interface AppointmentRepository extends CrudRepository<Appointment, Long>
 
     @Query(value = "select * from appointment where user_id = ?", nativeQuery = true)
     List<Appointment> findUserAppointments(Long userId);
-    @Query(value = "SELECT * FROM appointment where user_id = :userid and end_date = :date", nativeQuery = true)
+    @Query(value = "SELECT * FROM appointment where user_id = :userid and end_date >= :date and start_date <= :date", nativeQuery = true)
     List<Appointment> findUserEndDateAppointments(@Param("userid")  Long userId, @Param("date") String dateTime);
 }
